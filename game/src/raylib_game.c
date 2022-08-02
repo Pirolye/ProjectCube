37c6a027c7d546f9b2f8ad3c7d94525ded946627
pirolye <birolevente33@gmail.com>
2022-08-02 15:58:28 +0200

    - Trying to get cube controls in

--- game/src/raylib_game.c
+++ game/src/raylib_game.c
@@ -7,6 +7,7 @@
 ********************************************************************************************/
 
 #include "raylib.h"
+#include "raymath.h"
 
 //----------------------------------------------------------------------------------
 // Shared Variables Definition (global)
@@ -20,6 +21,8 @@ static const int screenWidth = 800;
 static const int screenHeight = 450;
 static Camera3D camera = { 0 };
 static Model mainCube;
+static Vector2 mousePosChange;
+static Vector2 cubeRot;
 
 static void UpdateDrawFrame(void);          // Update and draw one frame
 
@@ -28,18 +31,18 @@ int main(void)
 	SetConfigFlags(FLAG_MSAA_4X_HINT);  // Enable Multi Sampling Anti Aliasing 4x (if available)
 
     // Initialization
-    InitWindow(screenWidth, screenHeight, "Missing Cube -- WORKING TITLE");
+    InitWindow(1600, 900, "Missing Cube -- WORKING TITLE");
 
     InitAudioDevice();      // Initialize audio device
 
     
-    camera.position = (Vector3){ 10.0f, 10.0f, 10.0f }; // Camera position
+    camera.position = (Vector3){ 6.0f, 0.0f, 0.0f }; // Camera position
     camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };      // Camera looking at point
     camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
-    camera.fovy = 45.0f;                                // Camera field-of-view 
+    camera.fovy = 70.0f;                                // Camera field-of-view 
     camera.projection = CAMERA_PERSPECTIVE;                   // Camera mode type
 
-    SetCameraMode(camera, CAMERA_FREE); // Set a free camera mode
+    SetCameraMode(camera, CAMERA_CUSTOM); // Set a free camera mode
 
     mainCube            = LoadModel("game/content/model/smallCube/smallCube.obj");
     Texture2D mainCube_albedo = LoadTexture("game/content/model/smallCube/smallCube_albedo.png");
@@ -72,10 +75,19 @@ int main(void)
 // Update and draw game frame
 static void UpdateDrawFrame(void)
 {
-
     UpdateCamera(&camera);
 
     //UpdateMusicStream(music);  // This is used when we need to manually increment the music player. No use when no music is playing.
+
+	if (IsMouseButtonDown(MOUSE_BUTTON_LEFT))
+	{
+		HideCursor();
+
+		Vector2 delta = GetMouseDelta();
+
+		cubeRot.x = cubeRot.x + (delta.x / 10.0f);
+		cubeRot.y = cubeRot.y + (delta.y / 10.0f);
+	}
 
     BeginDrawing();
 
@@ -83,13 +95,16 @@ static void UpdateDrawFrame(void)
 
         BeginMode3D(camera);
 
-			DrawModel(mainCube, (Vector3){0.0f, 0.0f, 0.0f}, 1.0f, WHITE);
+		DrawModelEx(mainCube, (Vector3) { 0.0f, 0.0f, 0.0f }, (Vector3) { 1.0f, 1.0f, 1.0f }, (Vector3){}, (Vector3) { 1.0f, 1.0f, 1.0f }, WHITE);
 
             DrawGrid(10, 1.0f);
 
         EndMode3D();
 
         DrawFPS(10, 10);
+
+
+	ShowCursor;
 
     EndDrawing();
 }
