2866dbbafe2a532669eca4d8a68f9a490de6476d
Levente Biro <birolevente33@gmail.com>
2024-06-16 14:13:55 +0200

    Update entt_camera.h

--- src/entt_camera.h
+++ src/entt_camera.h
@@ -5,21 +5,6 @@
 
 ;
 /*
-void on_make(entt_camera* inEntt);
-
-void on_update(entt_camera* inEntt);
-void on_draw_2d(entt_camera* inEntt);
-void on_draw_3d(entt_camera* inEntt);
-
-void update_spatial_props(entt_camera* inEntt, Vector3 inNewPos, Vector3 inNewScale, graphene_quaternion_t* inNewRotation);
-void update_spatial_props(entt_camera* inEntt, Vector3 inNewPos, Vector3 inNewScale);
-void set_mode(entt_camera* inEntt, int inMode, bool isForEditorOnly);
-void transform_camera_by_delta(entt_camera* inEntt, Vector3 inPosition, Vector3 inRotation);
-void editor_camera_update_model_rotation();
-
-void on_destroy(entt_camera* inEntt);
-
-entt_camera* editor_try_select(entt_camera* inEntt);
 */
 
 #else
